c4ed4dd9f1de25b982ff1a2835a45f6923592d0b
TheNuggitMan <64538216+TheNuggitMan@users.noreply.github.com>
2020-06-18 22:52:43 +0100

    Updated referrerPolicy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getData(){
 fetch('http://api.open-notify.org/iss-now.json', {
-  referrerPolicy: "no-referrer"})
+  referrerPolicy: "origin"})
 .then(function (response) {
   return response.json();
 })
