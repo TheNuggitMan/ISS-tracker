c762b7ce675979b20844668ed9d51055fa5d144f
TheNuggitMan <64538216+TheNuggitMan@users.noreply.github.com>
2020-06-19 00:01:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function getData(){
-fetch('http://api.open-notify.org/iss-now.json')
+fetch('http://api.open-notify.org/iss-now.json', {
+  credentials: "include")}
 .then(function (response) {
   return response.json();
 })
